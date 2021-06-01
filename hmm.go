d7fd1b8dc26077a546900df883d5ed68868b6d84
Jonas Knobloch <jonas.knobloch@t-online.de>
2021-06-01 12:23:28 +0200

    Add Viterbi algorithm

--- hmm.go
+++ hmm.go
@@ -117,6 +117,10 @@ func main() {
 	fmt.Printf("[backward] P(V=aby) = %f\n", backward([]string{"a", "b", "y"}, h))
 	fmt.Printf("[backward] P(V=abby) = %f\n", backward([]string{"a", "b", "b", "y"}, h))
 	fmt.Printf("[backward] P(V=abbby) = %f\n", backward([]string{"a", "b", "b", "b", "y"}, h))
+
+	fmt.Printf("[viterbi] P(V=aby) = %s\n", viterbi([]string{"a", "b", "y"}, h))
+	fmt.Printf("[viterbi] P(V=abby) = %s\n", viterbi([]string{"a", "b", "b", "y"}, h))
+	fmt.Printf("[viterbi] P(V=abbby) = %s\n", viterbi([]string{"a", "b", "b", "b", "y"}, h))
 }
 
 func naive(V []string, h *hmm) float64 {
@@ -217,4 +221,58 @@ func backward(V []string, h *hmm) float64 {
 	}
 
 	return sum
+}
+
+func viterbi(V []string, h *hmm) []string {
+	T := make(map[int]map[string]float64)
+
+	for i := range V {
+		T[i+1] = make(map[string]float64)
+	}
+
+	psi := make(map[int]map[string]string)
+
+	for t := 2; t <= len(V); t++ {
+		psi[t] = make(map[string]string)
+	}
+
+	for _, q := range h.states {
+		T[1][q] = h.pEmission[q][V[0]] * h.pTransition[h.final][q]
+	}
+
+	for t := 2; t <= len(V); t++ {
+		for _, q := range h.states {
+			var max float64
+			var argMax string
+
+			for _, qq := range h.states {
+				if p := h.pTransition[qq][q] * T[t-1][qq]; p > max {
+					max = p
+					argMax = qq
+				}
+			}
+
+			T[t][q] = h.pEmission[q][V[t-1]] * max
+			psi[t][q] = argMax
+		}
+	}
+
+	var max float64
+	var argMax string
+
+	for _, q := range h.states {
+		if p := h.pTransition[q][h.final] * T[len(V)][q]; p > max {
+			max = p
+			argMax = q
+		}
+	}
+
+	Q := make([]string, len(V))
+	Q[len(V)-1] = argMax
+
+	for t := len(V) - 1; t >= 1; t-- {
+		Q[t-1] = psi[t+1][Q[t]]
+	}
+
+	return Q
 }
